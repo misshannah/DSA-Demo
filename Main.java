4a2f5e2633876ff202ca0106ef993340a2e54c4c
Hannah Olukoye <hannahotieno@gmail.com>
2020-09-09 12:06:21 +0000

    Task: Finding Nemo

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+
 class Main {
+  static String[] nemo = {"nemo"};
+  static String[] everyone = {"dory", "nigel","dory", "nigel","dory", "nigel","dory", "nigel"};
+  static long t1,t0;
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    findNemo(nemo);
+    t1 = System.currentTimeMillis();
+    
+    t0 = System.currentTimeMillis();
+
+  }
+  public static void findNemo(String [] arr) {
+    for (int i = 0; i < nemo.length; i ++){
+      if (nemo[i] == "nemo" )
+    System.out.println("Nemo exists");
+    }
+    System.out.println("Call to find Nemo took " + (t1 - t0) + " milliseconds");
   }
 }
