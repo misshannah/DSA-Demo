62f00818e8da23ef12e53deab4240cb273ec3d55
Hannah Olukoye <hannahotieno@gmail.com>
2020-09-15 07:15:55 +0000

    Constant time for o notation

--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
 import java.util.Arrays;
 
 class Main {
-  static String[] nemo = {"nemo"};
-  static String[] everyone = {"dory", "nigel","dory", "nigel","dory", "nigel","dory", "nigel"};
-  
+  // static String[] nemo = {"nemo"};
+  // static String[] everyone = {"dory", "nigel","dory", "nigel","dory", "nigel","dory", "nigel"};
+  static int[] boxes = {0, 1, 2, 3,4, 5};
   public static void main(String[] args) {
-    //findNemo(nemo);
-    String[] large = new String[1000];
-        Arrays.fill(large, "nemo");
+  //   //findNemo(nemo);
+  //   String[] large = new String[1000];
+  //       Arrays.fill(large, "nemo");
         
-   findNemo(large);
+  //  findNemo(large);
+logFirstTwoBoxes(boxes);
+ }
+  // public static void findNemo(String [] arr) {
+  //   for (int i = 0; i < nemo.length; i ++){
+  //   System.out.println("Nemo exists");
+  //   }
+  // }
+ public static void logFirstTwoBoxes(int [] arr) {
+System.out.println(boxes[0]);
+System.out.println(boxes[1]);
 
-  }
-  public static void findNemo(String [] arr) {
-    for (int i = 0; i < nemo.length; i ++){
-    System.out.println("Nemo exists");
-    }
-  }
+}
+
     
 }
